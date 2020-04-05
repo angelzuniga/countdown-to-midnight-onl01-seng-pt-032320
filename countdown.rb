07ee237f6c314bcb3b3f9cac3ea29897bbd6e934
Angel Zuniga <angelzuniga@outlook.com>
2020-04-05 00:46:11 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,9 +1,16 @@
-def countdown(x)
-  while x >= 0 
+def countdownn(x)
+  while x > 0 
   puts "#{x} SECOND(S)!"
-  x -= 1
+  x += -1
   
+  end
+ puts "HAPPY NEW YEARS!"
+end
 
-  return "HAPPY NEW YEAR!"
-  sleep (1) 
+def countdown_with_sleep
+  while y > 0 
+  puts "#{y} SECOND(S)!"
+  x += -1
+  end
+puts "HAPPY NEW YEARS!"
 end
